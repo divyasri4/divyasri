725f7dfee6da09343e9b4abbc72eadc02a08b85b
divyasri4 <39362932+divyasri4@users.noreply.github.com>
2018-05-17 12:12:06 +0530

    Add files via upload

---
+++ wrapperexp.java
@@ -0,0 +1,12 @@
+package com.may17;
+
+public class wrapperexp
+{
+public static void main(String[] args) 
+{
+	String a="10";
+	String b="20";
+	int c=Integer.parseInt(a)+Integer.parseInt(b);
+	System.out.println("the sum is"+c); 
+}
+}
